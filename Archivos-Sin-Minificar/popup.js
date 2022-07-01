f96344453057a458be85ee2a7ac33962dd8b42ae
Jose Gomez <josedavidgomezavila@gmail.com>
2022-07-01 10:54:12 -0500

    Se agrega soporte para mas pantallas

--- Archivos-Sin-Minificar/popup.js
+++ Archivos-Sin-Minificar/popup.js
@@ -1,9 +1,15 @@
 var btnAbrir = document.getElementById("abrir-web"),
+    btnAbrir12 = document.getElementById("abrir-web12"),
     overlay = document.getElementById("overlay"),
     popup = document.getElementById("popup"),
     btn_cerrar = document.getElementById("btn-cerrar");
 
 btnAbrir.addEventListener('click', function () {
+    overlay.classList.add('active');
+    popup.classList.add('active');
+})
+
+btnAbrir12.addEventListener('click', function () {
     overlay.classList.add('active');
     popup.classList.add('active');
 })
@@ -14,11 +20,17 @@ btn_cerrar.addEventListener('click', function () {
 })
 
 var btnAbrir2 = document.getElementById("abrir-app"),
+    btnAbrir22 = document.getElementById("abrir-app22"),
     overlay2 = document.getElementById("overlay2"),
     popup2 = document.getElementById("popup2"),
     btn_cerrar2 = document.getElementById("btn-cerrar2");
 
 btnAbrir2.addEventListener('click', function () {
+    overlay2.classList.add('active');
+    popup2.classList.add('active');
+})
+
+btnAbrir22.addEventListener('click', function () {
     overlay2.classList.add('active');
     popup2.classList.add('active');
 })
