f7ee2b9f9e01ae9affce36e945f8309e1603f81c
Jose David Gomez <josedavidgomezavila@gmail.com>
2023-02-17 19:59:20 -0500

    Se agregan nuevos proyectos

--- popup.js
+++ popup.js
@@ -1,1 +1,131 @@
-var btnAbrir=document.getElementById("abrir-web"),btnAbrir12=document.getElementById("abrir-web12"),overlay=document.getElementById("overlay"),popup=document.getElementById("popup"),btn_cerrar=document.getElementById("btn-cerrar");btnAbrir.addEventListener("click",function(){overlay.classList.add("active"),popup.classList.add("active")}),btnAbrir12.addEventListener("click",function(){overlay.classList.add("active"),popup.classList.add("active")}),btn_cerrar.addEventListener("click",function(){overlay.classList.remove("active"),popup.classList.remove("active")});var btnAbrir2=document.getElementById("abrir-app"),btnAbrir22=document.getElementById("abrir-app22"),overlay2=document.getElementById("overlay2"),popup2=document.getElementById("popup2"),btn_cerrar2=document.getElementById("btn-cerrar2");btnAbrir2.addEventListener("click",function(){overlay2.classList.add("active"),popup2.classList.add("active")}),btnAbrir22.addEventListener("click",function(){overlay2.classList.add("active"),popup2.classList.add("active")}),btn_cerrar2.addEventListener("click",function(){overlay2.classList.remove("active"),popup2.classList.remove("active")})
+//Funcion para abrir y cerrar el POPUP de la pagina web
+var btnAbrirWeb = document.getElementById("abrir-web"),
+    btnAbrirWeb2 = document.getElementById("abrir-web2"),
+    overlayWeb = document.getElementById("overlay_web"),
+    popupWeb = document.getElementById("popup_web"),
+    btnCerrarWeb = document.getElementById("btn_cerrar_web");
+
+btnAbrirWeb.addEventListener('click', function () {
+    overlayWeb.classList.add('active');
+    popupWeb.classList.add('active');
+})
+
+btnAbrirWeb2.addEventListener('click', function () {
+    overlayWeb.classList.add('active');
+    popupWeb.classList.add('active');
+})
+
+btnCerrarWeb.addEventListener('click', function () {
+    overlayWeb.classList.remove('active');
+    popupWeb.classList.remove('active');
+})
+
+//Funcion para abrir y cerrar el POPUP de la aplicacion en android
+var btnAbrirApp = document.getElementById("abrir-app"),
+    btnAbrirApp2 = document.getElementById("abrir-app2"),
+    overlayApp = document.getElementById("overlay_app"),
+    popupApp = document.getElementById("popup_app"),
+    btnCerrarApp = document.getElementById("btn_cerrar_app");
+
+btnAbrirApp.addEventListener('click', function () {
+    overlayApp.classList.add('active');
+    popupApp.classList.add('active');
+})
+
+btnAbrirApp2.addEventListener('click', function () {
+    overlayApp.classList.add('active');
+    popupApp.classList.add('active');
+})
+
+btnCerrarApp.addEventListener('click', function () {
+    overlayApp.classList.remove('active');
+    popupApp.classList.remove('active');
+})
+
+//Funcion para abrir y cerrar el POPUP del videojuego PiratesGo
+var btnAbrirPg = document.getElementById("abrir-pg"),
+    btnAbrirPg2 = document.getElementById("abrir-pg2"),
+    overlayPg = document.getElementById("overlay_pg"),
+    popupPg = document.getElementById("popup_pg"),
+    btnCerrarPg = document.getElementById("btn_cerrar_pg");
+
+btnAbrirPg.addEventListener('click', function () {
+    overlayPg.classList.add('active');
+    popupPg.classList.add('active');
+})
+
+btnAbrirPg2.addEventListener('click', function () {
+    overlayPg.classList.add('active');
+    popupPg.classList.add('active');
+})
+
+btnCerrarPg.addEventListener('click', function () {
+    overlayPg.classList.remove('active');
+    popupPg.classList.remove('active');
+})
+
+//Funcion para abrir y cerrar el POPUP del videojuego The Time Diamond adventure
+var btnAbrirTtda = document.getElementById("abrir-ttda"),
+    btnAbrirTtda2 = document.getElementById("abrir-ttda2"),
+    overlayTtda = document.getElementById("overlay_ttda"),
+    popupTtda = document.getElementById("popup_ttda"),
+    btnCerrarTtda = document.getElementById("btn_cerrar_ttda");
+
+btnAbrirTtda.addEventListener('click', function () {
+    overlayTtda.classList.add('active');
+    popupTtda.classList.add('active');
+})
+
+btnAbrirTtda2.addEventListener('click', function () {
+    overlayTtda.classList.add('active');
+    popupTtda.classList.add('active');
+})
+
+btnCerrarTtda.addEventListener('click', function () {
+    overlayTtda.classList.remove('active');
+    popupTtda.classList.remove('active');
+})
+
+//Funcion para abrir y cerrar el POPUP del videojuego Futuristic Skateboard
+var btnAbrirFs = document.getElementById("abrir-fs"),
+    btnAbrirFs2 = document.getElementById("abrir-fs2"),
+    overlayFs = document.getElementById("overlay_fs"),
+    popupFs = document.getElementById("popup_fs"),
+    btnCerrarFs = document.getElementById("btn_cerrar_fs");
+
+btnAbrirFs.addEventListener('click', function () {
+    overlayFs.classList.add('active');
+    popupFs.classList.add('active');
+})
+
+btnAbrirFs2.addEventListener('click', function () {
+    overlayFs.classList.add('active');
+    popupFs.classList.add('active');
+})
+
+btnCerrarFs.addEventListener('click', function () {
+    overlayFs.classList.remove('active');
+    popupFs.classList.remove('active');
+})
+
+//Funcion para abrir y cerrar el POPUP del videojuego Extravehicular Activity
+var btnAbrirEa = document.getElementById("abrir-ea"),
+    btnAbrirEa2 = document.getElementById("abrir-ea2"),
+    overlayEa = document.getElementById("overlay_ea"),
+    popupEa = document.getElementById("popup_ea"),
+    btnCerrarEa = document.getElementById("btn_cerrar_ea");
+
+btnAbrirEa.addEventListener('click', function () {
+    overlayEa.classList.add('active');
+    popupEa.classList.add('active');
+})
+
+btnAbrirEa2.addEventListener('click', function () {
+    overlayEa.classList.add('active');
+    popupEa.classList.add('active');
+})
+
+btnCerrarEa.addEventListener('click', function () {
+    overlayEa.classList.remove('active');
+    popupEa.classList.remove('active');
+})
